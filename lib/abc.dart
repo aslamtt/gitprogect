7812c53c9a40c91b955c12f9f0ac8c2b70a5a96f
user <shanub882@gamil.com>
2024-01-04 16:31:27 +0530

    newdart

---
+++ lib/abc.dart
@@ -0,0 +1,15 @@
+class MyNew extends StatefulWidget {
+  const MyNew({Key? key}) : super(key: key);
+
+  @override
+  State<MyNew> createState() => _MyNewState();
+}
+
+class _MyNewState extends State<MyNew> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
