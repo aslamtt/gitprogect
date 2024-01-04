ec812cd41a69558d0efba7032e36b1f82d2b9339
user <shanub882@gamil.com>
2024-01-04 17:17:39 +0530

    taskAdded

--- lib/abc.dart
+++ lib/abc.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class MyNew extends StatefulWidget {
   const MyNew({Key? key}) : super(key: key);
 
@@ -9,7 +12,59 @@ class _MyNewState extends State<MyNew> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.orange,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.green,
+                      ), Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.blue,
+                      ), Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.red,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 180,
+                        color: Colors.purple,
+                      ), Container(
+                        height: 300,
+                        width: 180,
+                        color: Colors.greenAccent,
+                      ),
+                      Container(
+                        height: 300,
+                        width: 180,
+                        color: Colors.yellow,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
